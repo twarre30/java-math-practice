1bff8441e3c1bf7ce9c304d4908795dc804ff6f5
Tiffany Warren <twarre30@gmail.com>
2022-05-24 12:26:10 -0500

    solution

--- MathApp.java
+++ MathApp.java
@@ -1,5 +1,32 @@
 public class MathApp {
   public static void main(String[] args) {
-    System.out.println("Answer goes here");
+    double job1Salary = 44808.50;
+    double job2Salary = 55800.10;
+    double max = Math.max(job1Salary, job2Salary);
+    System.out.println(max);
+
+    double carPrice = 10000.00;
+    double truckPrice = 25000.00;
+    double min = Math.max(carPrice, truckPrice);
+    System.out.println(min);
+
+    double radius = 7.25;
+    double areaOfCircle = Math.PI * radius * radius;
+    System.out.println(areaOfCircle);
+    
+    float num1 = 5.0f;
+    double squareRoot = Math.sqrt(num1);
+    System.out.println(squareRoot);
+
+    System.out.println(Math.sqrt(((5 * 10) + (85 * 50))));
+
+    float num2 = -3.8f;
+    float positiveValue = Math.abs(num2);
+    System.out.println(positiveValue);
+
+    System.out.println(Math.random());
+
+
+
   }
 }
